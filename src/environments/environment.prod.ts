433a546335c6ec694a672cbe325b928eee837f89
Jakub Ludwig <domaci.pirozek@gmail.com>
2022-05-25 08:29:03 +0000

    Added production URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
 	production: true,
-	apiUrl: '/api/v1/', // in src/proxy.conf.json is set proxy to localhost:5002
+	apiUrl: 'http://ec2-18-159-141-94.eu-central-1.compute.amazonaws.com/api/v1/', // in src/proxy.conf.json is set proxy to localhost:5002
 };
