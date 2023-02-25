e19372505b33a903dc194d35117a725cad076ca3
Vít Ludwig <vit.ludwig1@gmail.com>
2023-02-25 16:00:40 +0100

    mdc migration, style fixes

--- src/app/modules/admin/modules/transactions/components/user-transactions-list/user-transactions-list.component.ts
+++ src/app/modules/admin/modules/transactions/components/user-transactions-list/user-transactions-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import {ITransaction} from "../../services/transaction/types/ITransaction";
 import {TransactionService} from "../../services/transaction/transaction.service";
 import {UsersService} from "../../../../services/users/users.service";
