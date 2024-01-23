e1f6267919352cb68bb3122b8b803a042a95d95e
Vít Ludwig <vit.ludwig1@gmail.com>
2024-01-23 18:34:11 +0100

    changed api url to v1.1 on production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	apiUrl: 'https://cybertown-kredsys.eu/api/v1/', // in src/proxy.conf.json is set proxy to localhost:5002
+	apiUrl: 'https://cybertown-kredsys.eu/api/v1.1/', // in src/proxy.conf.json is set proxy to localhost:5002
 	debug: false,
 	walletApiSecret: 'trololoolodelsijelepsinezkratsiatojesteneviscostimdelamnabackenduapokudtohlectestaknapispirozkovizechcesdoITtymu',
 };
