698948a6d290d3b2c29624fe0e11d263d9a32105
Vít Ludwig <vit.ludwig@ceosdata.com>
2026-04-24 20:54:30 +0200

    fixes

--- src/app/modules/sale/components/dashboard/components/filter-panel/filter-panel.component.spec.ts
+++ src/app/modules/sale/components/dashboard/components/filter-panel/filter-panel.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { IsIncludedPipe } from '../../../../../../common/pipes/is-included/is-included.pipe';
 import { FilterPanelComponent } from './filter-panel.component';
 
 describe('FilterPanelComponent', () => {
@@ -8,7 +11,8 @@ describe('FilterPanelComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FilterPanelComponent ]
+      declarations: [ FilterPanelComponent ],
+      imports: [ CommonModule, MatIconModule, MatButtonModule, IsIncludedPipe ],
     })
     .compileComponents();
 
