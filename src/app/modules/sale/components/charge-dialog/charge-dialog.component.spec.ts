504fe2b6d5d59edc0b3e418bdeb8dab978ef3909
Vít Ludwig <vit.ludwig@ceosdata.com>
2026-05-12 17:37:30 +0200

    feat: replace hardcoded Kelímek button with dynamic charge items in ChargeDialogComponent

--- src/app/modules/sale/components/charge-dialog/charge-dialog.component.spec.ts
+++ src/app/modules/sale/components/charge-dialog/charge-dialog.component.spec.ts
@@ -1,37 +1,51 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
-import {NO_ERRORS_SCHEMA} from '@angular/core';
-import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-import {ChargeDialogComponent} from './charge-dialog.component';
-import {clearAllCaches} from '../../../../common/decorators/cache';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ChargeDialogComponent } from './charge-dialog.component';
+import { SettingsService } from '../../../../modules/admin/services/settings/settings.service';
+import { clearAllCaches } from '../../../../common/decorators/cache';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('ChargeDialogComponent', () => {
-	let component: ChargeDialogComponent;
-	let fixture: ComponentFixture<ChargeDialogComponent>;
+  let component: ChargeDialogComponent;
+  let fixture: ComponentFixture<ChargeDialogComponent>;
+  let mockSettingsService: jasmine.SpyObj<SettingsService>;
 
-	beforeEach(async () => {
-		clearAllCaches();
-		await TestBed.configureTestingModule({
-    declarations: [ChargeDialogComponent],
-    schemas: [NO_ERRORS_SCHEMA],
-    imports: [],
-    providers: [
+  beforeEach(async () => {
+    clearAllCaches();
+    mockSettingsService = jasmine.createSpyObj('SettingsService', ['getChargeItems']);
+    mockSettingsService.getChargeItems.and.returnValue(
+      Promise.resolve([{ label: 'Kelímek', amount: 60 }])
+    );
+
+    await TestBed.configureTestingModule({
+      declarations: [ChargeDialogComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
         { provide: MatDialogRef, useValue: {} },
         { provide: MAT_DIALOG_DATA, useValue: 0 },
+        { provide: SettingsService, useValue: mockSettingsService },
         provideHttpClient(withInterceptorsFromDi()),
         provideHttpClientTesting(),
-    ]
-}).compileComponents();
-	});
+      ],
+    }).compileComponents();
+  });
 
-	beforeEach(() => {
-		fixture = TestBed.createComponent(ChargeDialogComponent);
-		component = fixture.componentInstance;
-		fixture.detectChanges();
-	});
+  beforeEach(async () => {
+    fixture = TestBed.createComponent(ChargeDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    await fixture.whenStable();
+    fixture.detectChanges();
+  });
 
-	it('should create', () => {
-		expect(component).toBeTruthy();
-	});
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('loads charge items on init', () => {
+    expect(mockSettingsService.getChargeItems).toHaveBeenCalled();
+    expect(component['chargeItems']).toEqual([{ label: 'Kelímek', amount: 60 }]);
+  });
 });
--- src/app/modules/sale/components/charge-dialog/charge-dialog.component.ts
+++ src/app/modules/sale/components/charge-dialog/charge-dialog.component.ts
@@ -1,23 +1,29 @@
-import {Component, inject} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
-import {ConfigService} from "../../../../common/services/config/config.service";
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { IChargeItem } from '../../../../common/types/IChargeItem';
+import { SettingsService } from '../../../admin/services/settings/settings.service';
 
 @Component({
-    selector: 'app-charge-dialog',
-    templateUrl: './charge-dialog.component.html',
-    styleUrls: ['./charge-dialog.component.scss'],
-    standalone: false
+  selector: 'app-charge-dialog',
+  templateUrl: './charge-dialog.component.html',
+  styleUrls: ['./charge-dialog.component.scss'],
+  standalone: false
 })
-export class ChargeDialogComponent {
+export class ChargeDialogComponent implements OnInit {
   protected dialogRef = inject(MatDialogRef<ChargeDialogComponent>);
-  protected amount: number = inject(MAT_DIALOG_DATA)
-  private configService = inject(ConfigService);
+  protected amount: number = inject(MAT_DIALOG_DATA);
+  private settingsService = inject(SettingsService);
+  private cdr = inject(ChangeDetectorRef);
 
-	protected predefinedAmounts: number[] = [500, 800, 1000, 1500, 2000];
-  protected cruciblePrice = this.configService.config.cruciblePrice;
+  protected predefinedAmounts: number[] = [500, 800, 1000, 1500, 2000];
+  protected chargeItems: IChargeItem[] = [];
 
-	public submit(): void {
-		this.dialogRef.close(this.amount);
-	}
+  async ngOnInit(): Promise<void> {
+    this.chargeItems = await this.settingsService.getChargeItems();
+    this.cdr.detectChanges();
+  }
 
+  public submit(): void {
+    this.dialogRef.close(this.amount);
+  }
 }
